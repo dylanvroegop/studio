4dc2f91a34cb9e7b3f592fb798cd311bf5d0a76c
dylan vroegop <dylanvroegop2@gmail.com>
2025-12-03 21:37:34 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/materialen/page.tsx
+++ src/app/materialen/page.tsx
@@ -24,6 +24,7 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
