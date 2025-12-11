c1a1015f7be9085dcf3571a9a671ec8ae5106a3f
dylan vroegop <dylanvroegop2@gmail.com>
2025-12-11 12:26:26 +0000

    its working again. from the title in the topbar/header 'Kies een klus:'

--- src/app/offertes/[id]/klus/nieuw/page.tsx
+++ src/app/offertes/[id]/klus/nieuw/page.tsx
@@ -67,7 +67,7 @@ export default function NewJobPage() {
                     </Button>
                 </div>
                 <div className="text-center">
-                    <h1 className="font-semibold text-lg">Kies een klus:</h1>
+                    <h1 className="font-semibold text-lg">Kies een klus</h1>
                     <p className="text-xs text-muted-foreground">stap 2 van 6</p>
                 </div>
                 <div className="flex items-center justify-end">
