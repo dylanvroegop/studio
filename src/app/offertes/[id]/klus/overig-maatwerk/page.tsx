df7b5f62a0f4195511ea2979e0db8a55452a7587
dylan vroegop <dylanvroegop2@gmail.com>
2025-12-20 15:45:58 +0000

    edit only this page; /offertes/OZgnzW7jcgoBxcIzbkxO/klus/overig-maatwerk

--- src/app/offertes/[id]/klus/overig-maatwerk/page.tsx
+++ src/app/offertes/[id]/klus/overig-maatwerk/page.tsx
@@ -98,7 +98,7 @@ export default function OverigMaatwerkPage() {
           </Button>
         </div>
         <div className="text-center">
-            <h1 className="font-semibold text-lg">Maatwerk:</h1>
+            <h1 className="font-semibold text-lg">custom</h1>
             <p className="text-xs text-muted-foreground">stap 3 van 6</p>
         </div>
         <div className="flex items-center justify-end">
